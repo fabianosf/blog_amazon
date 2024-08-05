3bfb0f7043980db20c4f650e7d3921170e276cce
Fabiano Freitas <fabiano.freitas@gmail.com>
2024-08-05 12:02:11 -0300

    six commit blog_django

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from assignments.models import About
+from blog_main.forms import RegistrationForm
 from blogs.models import Blog, Category
 
 
@@ -7,8 +9,23 @@ def home(request):
     featured_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('updated_at')
     posts = Blog.objects.filter(is_featured=False, status='Published')
     
+    # Fetch about us
+    try:
+        about = About.objects.get()
+    except:
+        about = None
+    
     context = {        
         'featured_posts': featured_posts,
         'posts': posts,
+        'about': about,
     }
     return render(request, 'home.html', context)
+
+
+def register(request):
+    form = RegistrationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'register.html', context)
